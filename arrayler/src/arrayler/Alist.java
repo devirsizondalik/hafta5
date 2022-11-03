0908b9e52f4719a2cf5b097a9c826dee77df7946
aidata <aidata@10.121.3.189>
2022-11-03 09:28:12 +0300

    list

---
+++ arrayler/src/arrayler/Alist.java
@@ -0,0 +1,26 @@
+package arrayler;
+
+import java.util.ArrayList;
+
+public class Alist {
+
+	public static void main(String[] args) {
+		ArrayList<String> dizi=new ArrayList<String>();
+		dizi.add("red");
+		dizi.add("yellow");
+		dizi.add("green");
+		dizi.add(1,"orange");
+		for(int i=0; i<dizi.size(); i++)
+		{
+			System.out.println(dizi.get(i));
+		}
+		if(dizi.contains("red")) {
+			System.out.println("Dizi içinde red var ");
+		}
+		else
+			System.out.println("Dizi içinde red yok");
+		
+		System.out.println("Green Değerinin İndeksi " + dizi.indexOf("green"));
+	}
+
+}
